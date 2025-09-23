76713fdf8978359ad594fa4bf986a6e5e511e3ba
Mario López Fuentes <mlf00040@red.ujaen.es>
2025-09-23 18:14:57 +0200

    gitignore arreglado

--- cmake-build-debug/conan/build/Debug/generators/conandeps_legacy.cmake
+++
@@ -1,7 +0,0 @@
-message(STATUS "Conan: Using CMakeDeps conandeps_legacy.cmake aggregator via include()")
-message(STATUS "Conan: It is recommended to use explicit find_package() per dependency instead")
-
-find_package(glfw3)
-find_package(opengl_system)
-
-set(CONANDEPS_LEGACY  glfw  opengl::opengl )
